c32e87ccacab98b6f0a686c54ca67a72ead97a5b
Felix <felixsaiz97@gmail.com>
2021-12-23 13:35:11 +0000

    day 3 complete

--- aoc2021/day_3/main.py
+++ aoc2021/day_3/main.py
@@ -1,5 +1,5 @@
 import argparse
-
+from statistics import mode as stat_mode
 
 def parse_cli():
     parser = argparse.ArgumentParser()
@@ -7,22 +7,70 @@ def parse_cli():
     return parser.parse_args()
 
 
+def list_to_bin(digits):
+    return sum(d << i for i, d in enumerate(reversed(digits)))
+
+
+def mode(digits):
+    return None if digits.count(1) == digits.count(0) else stat_mode(digits)
+
+
+def check_bit_criteria_mc(binary, count, i, n):
+    if count/n > 0.5 and binary[i]:
+        return True
+    elif count/n < 0.5 and not binary[i]:
+        return True
+    elif count/n == 0.5 and binary[i]:
+        return True
+    else:
+        return False
+
+def check_bit_criteria(bit, common, setting):
+    # checks if the bit meets the bit criteria according to the setting.
+    result = bool(bit) if common == None else not (bit ^ common)
+
+    if setting == "mc":
+        return result
+    elif setting == "lc":
+        return not result
+    else:
+        ValueError("Setting does not exist.")
+
+
+def part_2_alg(report, n, setting):
+    for i in range(n):
+        common = mode([x[i] for x in report])
+        temp = [x for x in report if check_bit_criteria(x[i], common, setting)]
+        report = temp.copy()
+
+        if len(report) == 1:
+            return list_to_bin(report[0])
+
+    return None
+
+
 def main():
     with open(args.input) as file:
-        report = [list(line.strip()) for line in file.readlines()]
+        report = [[int(d) for d in line.strip()] for line in file.readlines()]
 
-    n = len(report)
-    # print(n)  # debug
-    # print(report)  # debug
-    z = zip(*report)
+    n = len(report[0])
 
-    counts = [item.count('1') for item in z]
+    gamma = list_to_bin(list(map(mode, zip(*report))))
 
-    answer = [1 if count/n > 0.5 else 0 for count in counts]
+    mask = sum(1 << i for i in range(n))
 
-    print(answer)
+    epsilon = mask ^ gamma
+
+    print(f"Power consumption = {gamma * epsilon}")
+
+    oxy = part_2_alg(report, n, 'mc')
+    co2 = part_2_alg(report, n, 'lc')
+
+    lf_supp = oxy * co2
+    print(f"{lf_supp = }")
 
 
 if __name__ == '__main__':
     args = parse_cli()
     main()
+
